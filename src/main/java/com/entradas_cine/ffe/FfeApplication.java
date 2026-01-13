59edf1a1d87550df0f48f090784fe39813930d46
2daw <mapl63@educa.madrid.org>
2026-01-13 18:05:41 +0100

    Subiendo estructura del proyecto y el pom.xml completo para todos

---
+++ src/main/java/com/entradas_cine/ffe/FfeApplication.java
@@ -0,0 +1,13 @@
+package com.entradas_cine.ffe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FfeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/entradas_cine/ffe/FfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entradas_cine.ffe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
